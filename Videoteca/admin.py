f6354c6e93dae19e52d8ca8a0c99b95fe9aee558
cclauwers <cclauwers@gmail.com>
2017-04-17 00:45:21 +0200

    c2 definir els modelsdins de admin.py

--- Videoteca/admin.py
+++ Videoteca/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+import models
+admin.site.register(models.Actor)
+admin.site.register(models.Client)
+admin.site.register(models.Videoteca)
+admin.site.register(models.Movie)
